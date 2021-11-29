c35996c1e5af7b3d200824125023823182073dd0
Sayyadayesha <sayyadayesha17@gmail.com>
2021-11-29 11:38:39 +0530

    init

--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,28 @@
+
+//Header.js
 import React from 'react';
 import './Header.css';
-import  SearchIcon from '@mui/icons-material/Search';
+import SearchIcon from '@mui/icons-material/Search';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import { useStateValue } from './StateProvider';
+
 function Header() {
+  const [{cart}, dispatch] = useStateValue();
+
     return (
         <div className="header">
           <Link to="/">
           <img className="header--logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="amazon"/>
           </Link>
+          
+          
           <div className="header--search">
           <input className="header--searchInput" type="text" />
           {/* Material UI */}
-          <SearchIcon className="header--searchIcon"/>
+          <SearchIcon className="header--searchIcon" />
           </div>  
-
+          
           <div className="header--nav">
             <div className="header--option">
             <span className="header--optionLineOne">Hello Guest</span>
@@ -30,15 +38,16 @@ function Header() {
             <span className="header--optionLineOne">Your</span>
             <span className="header--optionLineTwo">Prime</span>
             </div>
+          </div>  
 
-          </div> 
-          <Link to="/">
-<div className="header--optionBasket">
-   <ShoppingCartIcon/>
-   <span className="header--optionLineTwo header--basketCount">0</span>
-   
-</div>
-</Link>
+          <Link to="/checkout">
+          <div className="header--optionBasket">
+          <ShoppingCartIcon />
+          <span className="header--optionLineTwo header--basketCount">{cart.length}</span>
+          </div>
+          </Link>
+          
+          
         </div>
     )
 }
--- src/Product.js
+++ src/Product.js
@@ -1,21 +1,41 @@
+//Product.js
 import React from 'react';
 import'./Product.css';
+import { useStateValue } from './StateProvider';
 
-function Product({id, title, price, image}) {
+function Product({id, title, price, image }) {
+    const [{cart}, dispatch] = useStateValue();
+
+    console.log("In side Cart",cart);
+
+    const addToCart = () => {
+
+        dispatch( {
+            type: "ADD_TO_CART",
+            item: {
+            id: id,
+            title: title,
+            price: price,
+            image: image,
+            }
+        }
+        );
+   }
+
     return (
         <div className="product">
             <div className="product--info">
-            <p>{title}</p>
-
-            <p className="product--price">
-            <bold>₹</bold>
-            <strong>{price}</strong>
-            </p>
+                <p>{title}</p>
                 
-             <img className="product--image" src={image} alt="" /> 
-             </div>
-             <button className="product--button">Add to Cart</button>  
-             
+                <p className="product--price">
+                <bold>₹</bold>
+                <strong>{price}</strong>
+                </p>
+                
+                <img className="product--image" src={image} alt="" /> 
+            </div>
+             <button className="product--button" onClick = {addToCart} >Add to Cart</button>  
+            
         </div>
     )
 }
